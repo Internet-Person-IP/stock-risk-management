a026d046e4cfeadf4eeb2f0961587149526ad840
Internet-Person-IP <taquiyt@gmail.com>
2020-07-18 15:56:01 +0200

    Added 5.5% row and color change also made comma work

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,40 @@ function App() {
     TwoPercentage: 0, 
     Percentage30:0 ,
     Percentage37:0, 
-    Percentage44:0
+    Percentage44:0,
+    Percentage55: 0
   });
 
  const updateValues = (event: any) =>{
-   const DataPoint: number = event.target.value;
+   const DataPoint: number = parseFloatIgnoreCommas(event.target.value);
    
    setValues({
      StopLoss: RoundNumber(DataPoint*0.98),
      TwoPercentage: RoundNumber(DataPoint*1.023),
      Percentage30: RoundNumber(DataPoint*1.03),
      Percentage37: RoundNumber(DataPoint*1.037),
-     Percentage44: RoundNumber(DataPoint*1.044)
+     Percentage44: RoundNumber(DataPoint*1.044),
+     Percentage55: RoundNumber(DataPoint*1.055)
    });
  }
 
  const RoundNumber = (Data: number ) : number => {
-   return Number(Data.toFixed(2));
+   return Number(Data.toFixed(3));
  }
- const {StopLoss,TwoPercentage,Percentage30,Percentage37, Percentage44} = values
+ const parseFloatIgnoreCommas = (numb: string) : number => {
+   if(numb==="") return 0;
+  const numberNoCommas = numb.replace(/,/g, '.');
+  return parseFloat(numberNoCommas);
+}
+ 
+ const {
+  StopLoss,
+  TwoPercentage,
+  Percentage30,
+  Percentage37,
+  Percentage44,
+  Percentage55
+  } = values
   return (
 
     
@@ -47,7 +62,7 @@ function App() {
     </InputGroup.Append>
   </InputGroup>
 
-    <Table striped bordered hover variant="dark">
+    <Table striped bordered variant="dark">
   <thead>
     <tr >
       <th>Percentage</th>
@@ -71,7 +86,7 @@ function App() {
       <td className="valuepercentage">{Percentage30}</td>
       <td className="meaningofvalue">WE TAKE THOSE!!!</td>
     </tr>
-    <tr className="bg-info">
+    <tr className="percentage37">
       <td>+3,7%</td>
       <td className="valuepercentage">{Percentage37}</td>
       <td className="meaningofvalue">DON'T CUT! CONSISTENCY IS AT SERIOUS RISK</td>
@@ -79,7 +94,12 @@ function App() {
     <tr className="bg-warning">
       <td>+4,4%</td>
       <td className="valuepercentage">{Percentage44}</td>
-      <td className="meaningofvalue">Don't... risk..it. Don't be brave. Set stop loss at +2,3%</td>
+      <td className="meaningofvalue"><label className="text-dark"> Don't... risk..it.</label> Don't be brave. Set stop loss at +2,3%</td>
+    </tr>
+    <tr className="bg-white text-dark">
+      <td>5,5%</td>
+      <td className="valuepercentage">{Percentage55}</td>
+      <td className="meaningofvalue percentage55">SELL OUT. TAKE A BREAK. GTFO AND LOOK FOR INTRADAY OPPORTUNITIES.</td>
     </tr>
   </tbody>
 </Table>
