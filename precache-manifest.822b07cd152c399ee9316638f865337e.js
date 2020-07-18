7bfdd2dc3de02af16b5af05bcc668d78bd7b59c2
Internet-Person-IP <taquiyt@gmail.com>
2020-07-18 13:23:13 +0200

    Updates

---
+++ precache-manifest.822b07cd152c399ee9316638f865337e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfeb466e1e94487f9d63ce9683763fd",
+    "url": "/stock-risk-management/index.html"
+  },
+  {
+    "revision": "d80e882ea52cb735f33b",
+    "url": "/stock-risk-management/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8510ac7acfe3140a83f5",
+    "url": "/stock-risk-management/static/css/main.045058b7.chunk.css"
+  },
+  {
+    "revision": "d80e882ea52cb735f33b",
+    "url": "/stock-risk-management/static/js/2.4ea826d6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/stock-risk-management/static/js/2.4ea826d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8510ac7acfe3140a83f5",
+    "url": "/stock-risk-management/static/js/main.9d115cf3.chunk.js"
+  },
+  {
+    "revision": "bdb604b32b2479d8931d",
+    "url": "/stock-risk-management/static/js/runtime-main.e4559949.js"
+  }
+]);
